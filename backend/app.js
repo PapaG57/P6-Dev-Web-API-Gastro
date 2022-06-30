f2f55ae41aeb7cd02c74fe45ea68cbc039522c55
Florent GERARD <florentgerard@sfr.fr>
2022-06-30 14:11:26 +0200

    My first commit

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const signupRoutes = require('./routes/user');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(
+    'mongodb+srv://Papa_G:gf7rHACoeo3IpvEB@project6.9ba31x9.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/user', signupRoutes);
+
+module.exports = app;
